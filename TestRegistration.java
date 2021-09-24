4eae80301a428383081da9eabaf13a265fd64f40
SyafiqIdris <83348407+SyafiqIdris@users.noreply.github.com>
2021-09-24 20:55:23 +0800

    Add files via upload

---
+++ TestRegistration.java
@@ -0,0 +1,12 @@
+package featurefour;
+
+public class TestRegistration {
+
+	public static void main(String[] args) {
+		Registration one = new Registration(001, "Syafiq", "Testing");
+		
+		System.out.println(one.toString());
+		
+	}
+	
+}
